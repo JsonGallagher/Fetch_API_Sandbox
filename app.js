917fa90dc5ac6fa3188f3abb26fcbd07df606700
Jason <jasong13@gmail.com>
2019-05-10 10:24:54 -0600

    Final code for Fetch API Sandbox

---
+++ app.js
@@ -0,0 +1,80 @@
+document.getElementById('getText').addEventListener('click', getText);
+document.getElementById('getUsers').addEventListener('click', getUsers);
+document.getElementById('getPosts').addEventListener('click', getPosts);
+document.getElementById('addPost').addEventListener('submit', addPost);
+// //Retrieve data from txt file - Basic syntax
+// function getText() {
+//     fetch('sample.txt') 
+//         .then(function(res){
+//             return res.text();
+//         })
+//         .then(function(data) {
+//             console.log(data);
+//         });
+// }
+
+//Retrieve data from txt file - Using Arrow functions
+function getText() {
+    fetch('sample.txt')
+        .then((res) => res.text())
+        .then((data) => {
+            document.getElementById('output').innerHTML = data;
+        })
+        .catch((err) => console.log(err))
+};
+
+//Retrieve data from JSON file - Loop through data, output as unordered list
+function getUsers() {
+    fetch('users.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '<h2 class="mb-4">Users</h2>';
+            data.forEach(function(user){
+                output += `
+                    <ul class="list-group mb-3">
+                        <li class="list-group-item">ID: ${user.id}</li>
+                        <li class="list-group-item">Name: ${user.name}</li>
+                        <li class="list-group-item">Email: ${user.email}</li>
+                    </ul>
+                `;
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+}
+
+//Retrieve data from outside API
+function getPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '<h2 class="mb-4">Posts</h2>';
+            data.forEach(function(post){
+                output += `
+                   <div class="card card-body mb-3">    
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                   </div>
+                `;
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+}
+
+//Add post via form
+function addPost(e) {
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({title:title, body:body})
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+}
